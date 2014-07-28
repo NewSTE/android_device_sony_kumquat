1bf7659704cef76efd918d1ab34670822fa23b49
Gava97 <gava97@gmail.com>
2014-07-28 17:32:30 +0200

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@ $(call inherit-product, device/sony/kumquat/full_kumquat.mk)
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/products/common_full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST25i_1262-1120 BUILD_FINGERPRINT=SEMC/ST25i_1262-1120/ST25i:4.0.4/6.1.1.B.1.10/9bt3zw:user/release-keys PRIVATE_BUILD_DESC="ST25i-user 4.0.4 6.1.1.B.1.10 9bt3zw test-keys"
 
