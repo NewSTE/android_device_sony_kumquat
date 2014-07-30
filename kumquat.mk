4473384258d476a2964c24bb29f289314b4426e7
DevSwift <devswift@wp.pl>
2014-07-30 17:44:07 +0200

    Revert "Move properties from system.prop to device file"
    This reverts commit 2b72c49e8c885906d83ad00bd1137747d0cf5596.

--- kumquat.mk
+++ kumquat.mk
@@ -89,32 +89,3 @@ $(call inherit-product-if-exists, vendor/sony/kumquat/kumquat-vendor.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=240
-
-PRODUCT_PROPERTY_OVERRIDES += \
-rild.libpath=/system/lib/libu300-ril.so \
-rild.libargs=-c UNIX -n 2 -p /dev/socket/at_core -s /dev/socket/at_core -i rmnet \
-ro.opengles.version=131072 \
-debug.sf.hw=1 \
-hwui.render_dirty_regions=false \
-ro.semc.sols.product-code=105 \
-ro.semc.sols.company-code=5 \
-ste.nmf.autoidle=1 \
-ste.dbus.bus.address=unix:path=/dev/socket/dbus_ste \
-ro.telephony.call_ring.multiple=false \
-ro.semc.product.user_storage=emmc_only \
-ro.build.characteristics=nosdcard \
-ro.nfc.on.default=false \
-ro.nfc.se.sim.enable=true \
-ro.nfc.se.smx.enable=false \
-ro.nfc.icon.enable=true \
-ro.nfc.vendor.name=nxp \
-ste.video.dec.mpeg4.in.size=8192 \
-ste.video.enc.out.buffercnt=5 \
-ste.video.dec.recycle.delay=1 \
-ste.special_fast_dormancy=false \
-ste.video.decoder.max.hwmem=0x3600000 \
-ste.video.decoder.max.res=1080p \
-ste.video.decoder.h264.max.lev=4.2 \
-persist.audio.hp=true \
-ro.service.swiqi.supported=false \
-persist.service.swiqi.enable=0
